cb703cb4213b7579659a39becbe4788ff33fc5f6
Alex Harvey <alex@razorconsulting.com.au>
2016-04-11 01:46:19 +1000

    Add missing facts from spec tests.

--- spec/classes/disable_transparent_hugepage_spec.rb
+++ spec/classes/disable_transparent_hugepage_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe 'disable_transparent_hugepage' do
+  let(:facts) {{
+    :osfamily                  => 'RedHat',
+    :operatingsystemmajrelease => '7',
+  }}
   it {
     is_expected.to compile.with_all_deps
   }
