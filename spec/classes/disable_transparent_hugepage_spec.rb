4731a59d2299da345ba5af6258917c3959335589
Jonathan Gray <jonthan.gray@milsoft.com>
2016-05-23 10:34:57 -0500

    Fix test failure

--- spec/classes/disable_transparent_hugepage_spec.rb
+++ spec/classes/disable_transparent_hugepage_spec.rb
@@ -4,6 +4,7 @@ describe 'disable_transparent_hugepage' do
   let(:facts) {{
     :osfamily                  => 'RedHat',
     :operatingsystemmajrelease => '7',
+    :tuned_base_profile        => 'virtual-guest',
   }}
   it {
     is_expected.to compile.with_all_deps
