b06f493f73262411efc9d3ca73f74f0a2b7bc133
Alex Harvey <alex@razorconsulting.com.au>
2016-05-28 16:41:28 +1000

    Add another test

--- spec/classes/disable_transparent_hugepage_spec.rb
+++ spec/classes/disable_transparent_hugepage_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe 'disable_transparent_hugepage' do
-  let(:facts) {{
-    :osfamily                  => 'RedHat',
-    :operatingsystemmajrelease => '7',
-  }}
-  it {
-    is_expected.to compile.with_all_deps
-  }
+  context 'EL7' do
+    let(:facts) {{
+      :osfamily                  => 'RedHat',
+      :operatingsystemmajrelease => '7',
+    }}
+    it {
+      is_expected.to compile.with_all_deps
+    }
+  end
+
+  context 'Debian' do
+    let(:facts) {{
+      :osfamily                  => 'Debian',
+      :operatingsystemmajrelease => '8',
+    }}
+    it {
+      is_expected.to compile.with_all_deps
+    }
+  end
 end
